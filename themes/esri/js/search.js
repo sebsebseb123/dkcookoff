262c1662403c81d6b71eec9912b11ab1ad574e8e
Sebastian Lesch <sebastian.lesch@gmail.com>
2012-10-02 10:57:14 -0400

    Still removing stuff...

--- themes/esri/js/search.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function() {
-  jQuery('a.facetapi-active').parents('li').addClass('facetapi-active');
-});
--- themes/esri/templates/blocks/menu/block--multiblock--3.tpl.php
+++
@@ -1,8 +0,0 @@
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?>">
-
-<?php if ($block->subject): ?>
-  <a href="#" name="footer-nav" id="footer-nav" class="element-invisible"><h2><?php print $block->subject ?></h2></a>
-<?php endif;?>
-
-  <div class="content"><?php print $content ?></div>
-</div>
